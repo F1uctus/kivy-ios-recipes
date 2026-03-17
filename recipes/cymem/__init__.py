3694ecba685b75c83ab651e278581a08db366daa
Ilya I. Nikitin <ilya.i.nikitin@proton.me>
2026-03-18 00:25:09 +0300

    Cymem: upgrade Cython in hostpython prerequisites
    cymem 2.0.13 uses Cython features not present in the default 3.0.11 pin. Upgrade Cython in the cymem recipe before build_ext so cythonization succeeds.

--- recipes/cymem/__init__.py
+++ recipes/cymem/__init__.py
@@ -1,10 +1,17 @@
-from kivy_ios.toolchain import CythonRecipe
+import sh
+
+from kivy_ios.toolchain import CythonRecipe, shprint
 
 
 class CymemRecipe(CythonRecipe):
     version = "2.0.13"
     url = "https://files.pythonhosted.org/packages/source/c/cymem/cymem-{version}.tar.gz"
     depends = ["python3"]
+
+    def install_hostpython_prerequisites(self):
+        super().install_hostpython_prerequisites()
+        python = sh.Command(self.ctx.hostpython)
+        shprint(python, "-m", "pip", "install", "--upgrade", "cython")
 
 
 recipe = CymemRecipe()
