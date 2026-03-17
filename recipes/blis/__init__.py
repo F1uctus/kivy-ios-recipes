52e063e6d2db9f9a9584b08bbadbc2c565647603
Ilya I. Nikitin <ilya.i.nikitin@proton.me>
2026-03-17 20:36:30 +0300

    Blis: set iOS sysconfig vars for build_ext
    Force distutils to use iOS sysconfig/platform tags when running under hostpython by setting _PYTHON_HOST_PLATFORM and _PYTHON_SYSCONFIGDATA_NAME. This avoids producing macOS-tagged build artifacts that break biglink on iOS.

--- recipes/blis/__init__.py
+++ recipes/blis/__init__.py
@@ -18,6 +18,12 @@ class BlisRecipe(CythonRecipe):
     def get_recipe_env(self, plat):
         env = super().get_recipe_env(plat)
         env["BLIS_ARCH"] = "x86_64" if plat.arch == "x86_64" else "generic"
+        # Ensure distutils uses iOS sysconfig/platform tags (not host macOS tags)
+        # when running build_ext under hostpython.
+        sdk = env.get("PLATFORM_SDK") or "iphoneos"
+        arch = env.get("ARCH") or plat.arch
+        env["_PYTHON_HOST_PLATFORM"] = f"ios-13.0-{arch}-{sdk}"
+        env["_PYTHON_SYSCONFIGDATA_NAME"] = "_sysconfigdata__ios_darwin"
         return env
 
 
