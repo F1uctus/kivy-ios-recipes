1370c9ec14e64b1c56de28d576dc8119d7acae7f
Ilya I. Nikitin <ilya.i.nikitin@proton.me>
2026-03-18 00:39:36 +0300

    Cymem: force iOS host platform sysconfig env
    Set _PYTHON_HOST_PLATFORM and _PYTHON_SYSCONFIGDATA_* for cymem so setuptools uses the iOS target sysconfig and generates biglink-compatible outputs.

--- recipes/cymem/__init__.py
+++ recipes/cymem/__init__.py
@@ -13,6 +13,18 @@ class CymemRecipe(CythonRecipe):
         python = sh.Command(self.ctx.hostpython)
         shprint(python, "-m", "pip", "install", "--upgrade", "cython")
 
+    def get_recipe_env(self, plat):
+        env = super().get_recipe_env(plat)
+        sdk = env.get("PLATFORM_SDK") or "iphoneos"
+        arch = env.get("ARCH") or plat.arch
+        host_plat = f"ios-13.0-{arch}-{sdk}"
+        env["_PYTHON_HOST_PLATFORM"] = host_plat
+        env["_PYTHON_SYSCONFIGDATA_NAME"] = "_sysconfigdata__ios_darwin"
+        env["_PYTHON_SYSCONFIGDATA_PATH"] = (
+            f"{self.ctx.build_dir}/python3/{sdk}-{arch}/Python-3.13.12/build/lib.{host_plat}-3.13"
+        )
+        return env
+
 
 recipe = CymemRecipe()
 
